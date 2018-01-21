5b5602839a18b5ec8b43b5d9eb830e21ea1cda0e
Sietse van der Meer <sietsevander@immermeer.eu>
2018-01-21 16:54:17 +0100

    Uncomplicate path settings, add cleanup toggle

--- profittrailer-jsonbackupper.bat
+++ profittrailer-jsonbackupper.bat
@@ -2,24 +2,28 @@
 
 for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
 
+:: it is time
 set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
 set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
 set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
 set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
 
-set "source_drive=C:"
-set "backup_drive=Z:"
-set "backup_dir=profittrailer\jsonbackups"
+:: backups go here
+set "backup_path=Z:\profittrailer\jsonbackups"
+
+:: delete backups older than this many days (set false to disable cleanup)
 set "backup_history=3"
 
-set "bittrex_name=PTBTCBittrex"
-set "bittrex_path=%source_drive%\%bittrex_name%"
-set "bittrex_backup=%backup_drive%\%backup_dir%\bittrex\btc"
-type "%bittrex_path%\ProfitTrailerData.json" >> "%bittrex_backup%\ProfitTrailerData-%bittrex_name%-%fullstamp%.json"
-forfiles /p "%bittrex_backup%" /s /m *.json /D -%backup_history% /C "cmd /c del @path"
+:: Trex
+set "bittrex_key=BittrexBTC"
+set "bittrex_path=C:\PTBTCBittrex"
+set "bittrex_backup=%backup_path%\bittrex\btc"
+type "%bittrex_path%\ProfitTrailerData.json" >> "%bittrex_backup%\ProfitTrailerData-%bittrex_key%-%fullstamp%.json"
+if %backup_history% neq false forfiles /p "%bittrex_backup%" /s /m *.json /D -%backup_history% /C "cmd /c del @path"
 
-set "binance_name=PTBTCBinance"
-set "binance_path=%source_drive%\%binance_name%"
-set "binance_backup=%backup_drive%\%backup_dir%\binance\btc"
-type "%binance_path%\ProfitTrailerData.json" >> "%binance_backup%\ProfitTrailerData-%binance_name%-%fullstamp%.json"
-forfiles /p "%binance_backup%" /s /m *.json /D -%backup_history% /C "cmd /c del @path"
+:: Nancy
+set "binance_key=BinanceBTC"
+set "binance_path=C:\PTBTCBinance"
+set "binance_backup=%backup_path%\binance\btc"
+type "%binance_path%\ProfitTrailerData.json" >> "%binance_backup%\ProfitTrailerData-%binance_key%-%fullstamp%.json"
+if %backup_history% neq false forfiles /p "%binance_backup%" /s /m *.json /D -%backup_history% /C "cmd /c del @path"
